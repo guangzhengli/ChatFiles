c8b4e377880df4e31760b680bdaaec1edc797312
guangzhengli <iguangzhengli@gmail.com>
2023-03-29 20:16:24 +0800

    feat: use llama_index to support chat with file.

--- chatfile/src/chatfile.py
+++ chatfile/src/chatfile.py
@@ -1,34 +1,38 @@
-from prompt import get_prompt_template
 import os
-import openai
 from pathlib import Path
-from llama_index import GPTSimpleVectorIndex, LLMPredictor, SimpleDirectoryReader
+
+import openai
 from langchain.chat_models import ChatOpenAI
+
+from llama_index import GPTSimpleVectorIndex, LLMPredictor, SimpleDirectoryReader
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY
 
-index_cache_file_dir = Path('../data/files/')
+index_cache_file_dir = Path('./documents')
 
 llm_predictor = LLMPredictor(llm=ChatOpenAI(
     temperature=0.2, model_name="gpt-3.5-turbo"))
 
-def create_llama_index(file):
-    index_name = get_index_name_from_file(file)
-    documents = SimpleDirectoryReader(input_files=[file]).load_data()
+
+def create_llama_index(filepath):
+    index_name = get_index_name_from_file(filepath)
+    documents = SimpleDirectoryReader(input_files=[filepath]).load_data()
     index = GPTSimpleVectorIndex(documents)
     index.save_to_disk(index_cache_file_dir / index_name)
     return index_name
 
-def get_answer_from_llama_index(messages, index_name):
-    index = get_index_from_file_cache(index_name)
-    prompt = get_prompt_template()
-    return index.query("\n".join(messages), llm_predictor=llm_predictor, text_qa_template=prompt)
 
-def get_index_name_from_file(file: str):
-    file_md5_with_extension = str(Path(file).relative_to(index_cache_file_dir).name)
+def get_answer_from_llama_index(text, index_name):
+    index = get_index_from_file_cache(index_name)
+    return index.query("\n".join(text), llm_predictor=llm_predictor)
+
+
+def get_index_name_from_file(filepath):
+    file_md5_with_extension = str(Path(filepath).relative_to(index_cache_file_dir).name)
     file_md5 = file_md5_with_extension.split('.')[0]
     return file_md5 + '.json'
+
 
 def get_index_from_file_cache(name):
     file_cache_file = index_cache_file_dir / name
--- chatfile/src/server.py
+++ chatfile/src/server.py
@@ -1,18 +1,51 @@
-from flask import Flask, request
+import os
+
+from flask import Flask, request, make_response, jsonify
+from werkzeug.utils import secure_filename
 
 from chatfile import create_llama_index, get_answer_from_llama_index
-app = Flask(__name__)
-	
-@app.route('/upload', methods = ['POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      return create_llama_index(f)
 
-@app.route('/query', methods = ['GET'])
+app = Flask(__name__)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "Please send a POST request with a file", 400
+    filepath = None
+    try:
+        uploaded_file = request.files["file"]
+        filename = secure_filename(uploaded_file.filename)
+        filepath = os.path.join('./documents', os.path.basename(filename))
+
+        uploaded_file.save(filepath)
+
+        return create_llama_index(filepath)
+    except Exception as e:
+        # cleanup temp file
+        if filepath is not None and os.path.exists(filepath):
+            os.remove(filepath)
+        return "Error: {}".format(str(e)), 500
+
+
+@app.route('/query', methods=['GET'])
 def query_from_llama_index():
-   message = request.args.get('message')
-   return get_answer_from_llama_index(message)
-		
+    text = request.args.get('text')
+    index_name = request.args.get('index_name')
+    answer = get_answer_from_llama_index(text, index_name)
+    response_json = {
+        "text": str(answer),
+        "sources": [{"text": str(x.source_text),
+                     "similarity": round(x.similarity, 2),
+                     "doc_id": str(x.doc_id),
+                     "start": x.node_info['start'],
+                     "end": x.node_info['end']
+                    } for x in answer.source_nodes]
+    }
+    return make_response(jsonify(response_json)), 200
+
+
 if __name__ == '__main__':
-   app.run(debug = True)
+    if not os.path.exists('./documents'):
+        os.makedirs('./documents')
+    app.run(port=5001, debug=True)
