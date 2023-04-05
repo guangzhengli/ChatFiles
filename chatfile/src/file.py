44943117b865ae37b2e05a561826c06e71a25c50
guangzhengli <iguangzhengli@gmail.com>
2023-04-05 19:38:47 +0800

    refactor: add file util and llm file.

---
+++ chatfile/src/file.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+index_path = './documents'
+index_file_dir = Path(index_path)
+
+
+def get_index_name_from_file_name(file_name):
+    file_with_type = str(Path(file_name).relative_to(index_file_dir).name)
+    file_index_name = file_with_type.split('.')[0]
+    return get_index_name_with_json_extension(file_index_name)
+
+
+def get_index_name_without_json_extension(index_name):
+    return index_name.replace(".json", "")
+
+
+def get_index_name_with_json_extension(index_name):
+    return index_name + '.json'
+
+
+def get_index_filepath(index_name):
+    return index_file_dir / index_name
+
+
+def get_index_path():
+    return index_path
+
+
+def check_index_file_exists(index_name):
+    return get_index_filepath(index_name).is_file()
+
+
+def check_index_exists(file_name):
+    index_name = get_index_name_from_file_name(file_name)
+    return check_index_file_exists(index_name)
---
+++ chatfile/src/llm.py
@@ -0,0 +1,24 @@
+import os
+import openai
+from langchain.chat_models import ChatOpenAI
+from llama_index import LLMPredictor, GPTSimpleVectorIndex
+
+from src.file import check_index_file_exists, get_index_filepath
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+openai.api_key = OPENAI_API_KEY
+
+llm_predictor = LLMPredictor(llm=ChatOpenAI(
+    temperature=0.2, model_name="gpt-3.5-turbo"))
+
+
+def get_llm_predictor():
+    return llm_predictor
+
+
+def get_index_by_index_name(index_name):
+    if check_index_file_exists(index_name) is False:
+        return None
+    index_filepath = get_index_filepath(index_name)
+    index = GPTSimpleVectorIndex.load_from_disk(index_filepath)
+    return index
