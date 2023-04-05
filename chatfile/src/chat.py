e41436130c597a72a43df79e835db9f4e9ce0fb3
guangzhengli <iguangzhengli@gmail.com>
2023-04-05 19:39:20 +0800

    feat: return index name without json extension.

---
+++ chatfile/src/chat.py
@@ -0,0 +1,24 @@
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+
+from src.file import get_index_filepath, get_index_name_from_file_name, check_index_file_exists, \
+    get_index_name_without_json_extension
+from src.llm import get_llm_predictor, get_index_by_index_name
+from src.prompt import get_prompt
+
+
+def check_llama_index_exists(file_name):
+    index_name = get_index_name_from_file_name(file_name)
+    return check_index_file_exists(index_name)
+
+
+def create_llama_index(filepath):
+    index_name = get_index_name_from_file_name(filepath)
+    documents = SimpleDirectoryReader(input_files=[filepath]).load_data()
+    index = GPTSimpleVectorIndex(documents)
+    index.save_to_disk(get_index_filepath(index_name))
+    return get_index_name_without_json_extension(index_name)
+
+
+def get_answer_from_llama_index(text, index_name):
+    index = get_index_by_index_name(index_name)
+    return index.query(text, llm_predictor=get_llm_predictor(), text_qa_template=get_prompt())
--- chatfile/src/server.py
+++ chatfile/src/server.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response
 from werkzeug.utils import secure_filename
 
-from chatfile import create_llama_index, get_answer_from_llama_index
+from chat import create_llama_index, get_answer_from_llama_index, check_llama_index_exists
+from src.file import get_index_path, get_index_name_from_file_name, check_index_file_exists, \
+    get_index_name_with_json_extension
 
 app = Flask(__name__)
 
@@ -15,12 +17,21 @@ def upload_file():
     filepath = None
     try:
         uploaded_file = request.files["file"]
-        filename = secure_filename(uploaded_file.filename)
-        filepath = os.path.join('./documents', os.path.basename(filename))
+        filename = uploaded_file.filename
+        filepath = os.path.join(get_index_path(), os.path.basename(filename))
+
+        if check_llama_index_exists(filepath) is True:
+            return get_index_name_from_file_name(filepath)
 
         uploaded_file.save(filepath)
 
-        return create_llama_index(filepath)
+        index_name = create_llama_index(filepath)
+
+        # cleanup temp file
+        if filepath is not None and os.path.exists(filepath):
+            os.remove(filepath)
+
+        return index_name
     except Exception as e:
         # cleanup temp file
         if filepath is not None and os.path.exists(filepath):
@@ -32,6 +43,11 @@ def upload_file():
 def query_from_llama_index():
     message = request.args.get('message')
     index_name = request.args.get('indexName')
+    index_name = get_index_name_with_json_extension(index_name)
+
+    if check_index_file_exists(index_name) is False:
+        return "Index file does not exist", 404
+
     answer = get_answer_from_llama_index(message, index_name)
     return make_response(str(answer.response)), 200
 
