335255079052a83f58b205703af7779ffd3e6f96
guangzhengli <iguangzhengli@gmail.com>
2023-04-05 19:39:45 +0800

    fix: fix runtime warning.

--- chatfiles-ui/components/Chat/Upload.tsx
+++ chatfiles-ui/components/Chat/Upload.tsx
@@ -33,7 +33,7 @@ export const Upload = ({onIndexChange, handleIsUploading}: Props) => {
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
                     <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor"
                          viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                               d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
                     </svg>
                     <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or
