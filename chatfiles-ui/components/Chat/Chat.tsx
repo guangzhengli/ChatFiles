91ff626fd3907c25972c6cf8b56da4cb22e71f65
guangzhengli <iguangzhengli@gmail.com>
2023-04-10 00:31:12 +0800

    fix: modify error message when upload failure.

--- chatfiles-ui/components/Chat/Chat.tsx
+++ chatfiles-ui/components/Chat/Chat.tsx
@@ -183,7 +183,7 @@ export const Chat: FC<Props> = memo(
                           <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
                           <span className="sr-only">Error</span>
                           <div className="ml-3 text-sm font-medium">
-                          ${errorMsg} <a href="#" className="font-semibold underline hover:no-underline">example link</a>. Give it a click if you like.
+                            ${errorMsg}.
                           </div>
                           <button type="button" className="ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-2" aria-label="Close">
                             <span className="sr-only">Close</span>
